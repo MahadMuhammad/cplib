cd99b29650d3f03904d0da86fe5ecb6da9038020
Muhammad Mahad <mahadtxt@gmail.com>
2024-04-07 12:55:57 +0500

    Added First Debug Header

---
+++ algo/debug.h
@@ -0,0 +1,31 @@
+#ifndef MAHAD_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+#endif
+
+template <typename A, typename B>
+ostream &operator<<(ostream &os, const pair<A, B> &p) {
+  return os << '(' << p.first << ", " << p.second << ')';
+}
+template <typename T_container, typename T = typename enable_if<
+                                    !is_same<T_container, string>::value,
+                                    typename T_container::value_type>::type>
+ostream &operator<<(ostream &os, const T_container &v) {
+  os << '{';
+  string sep;
+  for (const T &x : v) os << sep << x, sep = ", ";
+  return os << '}';
+}
+
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T) {
+  cerr << ' ' << H;
+  dbg_out(T...);
+}
+
+#ifdef MAHAD_DEBUG
+#define dbg(...) cout << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...) ((void)0)
+#endif
