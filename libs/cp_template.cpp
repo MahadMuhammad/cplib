a5c595fbde6249b4ddb5ebc56ecf8126d8f852dc
Muhammad Mahad <mahadtxt@gmail.com>
2024-04-07 13:10:14 +0500

    Added My CP template

---
+++ libs/cp_template.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef MAHAD_DEBUG
+#include "algo/debug.h"
+#else 
+#define debug(...) ((void)0) 
+#endif
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+   
+    unsigned int TC;
+    cin >> TC;
+    while(TC--){
+        
+    }
+    return 0;
+}
