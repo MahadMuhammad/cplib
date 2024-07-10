01ecbb5a276508421a4a9b03a5f6c885e35aafea
Muhammad Mahad <mahadtxt@gmail.com>
2024-07-11 00:03:22 +0500

    Update CP template and debug macros
    Refactor the CP template to use `dbg` instead of `debug`.
    - Added a welcome message in the template (this was already present in `debug.h`).

--- libs/cp_template.cpp
+++ libs/cp_template.cpp
@@ -4,14 +4,15 @@ using namespace std;
 #ifdef MAHAD_DEBUG
 #include "algo/debug.h"
 #else 
-#define debug(...) ((void)0) 
+#define dbg(...) ((void)0) 
+#define welcome(...) ((void)0)
 #endif
 
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-   
+
     unsigned int TC;
     cin >> TC;
     while(TC--){
